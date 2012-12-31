6732262debafe78b7feb29fd4ecec4fcacb1c794
chesterli0130 <chesterli0130@gmail.com>
2012-12-30 21:34:08 -0500

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -33,41 +33,41 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		
-		    if(root==null)
-		        return result;
+		if(root==null)
+			return result;
 			
-		    ArrayList<Integer> list = new ArrayList<Integer>();
-    		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-		    int current = 1;
-		    int next = 0;
-		    queue.add(root);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		int current = 1;
+		int next = 0;
+		queue.add(root);
 		
-		    while(!queue.isEmpty()){
-		        TreeNode n = queue.remove();
-			      list.add(n.val);
+		while(!queue.isEmpty()){
+			TreeNode n = queue.remove();
+			list.add(n.val);
 			
-			      if(n.left != null){
-				      queue.add(n.left);
-				      next++;
-			      }
+			if(n.left != null){
+				queue.add(n.left);
+				next++;
+			}
 			
-			      if(n.right != null){
-				      queue.add(n.right);
-				      next++;
-			      }
+			if(n.right != null){
+				queue.add(n.right);
+				next++;
+			}
 			
-    			  current--;
-    			  if(current==0){
-    				  current = next;
-    				  next = 0;
-    				  result.add(list);
-    				  list = new ArrayList<Integer>();
-    			  }
-		    }
+			current--;
+			if(current==0){
+				current = next;
+				next = 0;
+				result.add(list);
+				list = new ArrayList<Integer>();
+			}
+		}
 		
-		    return result;
+		return result;
     }
 }
 
